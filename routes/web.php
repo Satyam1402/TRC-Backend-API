5c69b039e069602f58da556442535593c2c6d66e
Satyam1402 <satyamkprajapati2001@gmail.com>
2025-05-10 20:18:10 +0530

    cosmetics

--- routes/web.php
+++ routes/web.php
@@ -73,19 +73,10 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::resource('announcements', AnnouncementController::class);
 });
 
-Route::get('/clear-config', function () {
-    Artisan::call('config:clear');
-    Artisan::call('route:clear');
-    Artisan::call('cache:clear');
-    Artisan::call('view:clear');
-    return response()->json(['message' => 'All caches cleared!']);
-});
-// Route::middleware(['auth:sanctum', 'verified'])->group(function () {
-//     Route::get('all/users', [All_Users_data_Controller::class, 'index'])->name('daily_reports.index');
-//     Route::get('user/data', [All_Users_data_Controller::class, 'getData'])->name('daily_reports.data');
-//     Route::get('user/add', [All_Users_data_Controller::class, 'add'])->name('daily_reports.add');
-//     Route::post('user/store', [All_Users_data_Controller::class, 'store'])->name('daily_reports.store');
-//     Route::get('user/show/{id}', [All_Users_data_Controller::class, 'show'])->name('daily_reports.show');
-//     Route::post('user/update/{id}', [All_Users_data_Controller::class, 'update'])->name('daily_reports.update');
-//     Route::get('user/destroy/{id}', [All_Users_data_Controller::class, 'destroy'])->name('daily_reports.destroy');
+// Route::get('/clear-config', function () {
+//     Artisan::call('config:clear');
+//     Artisan::call('route:clear');
+//     Artisan::call('cache:clear');
+//     Artisan::call('view:clear');
+//     return response()->json(['message' => 'All caches cleared!']);
 // });
